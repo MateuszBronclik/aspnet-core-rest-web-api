0cf79768364f9d8d6fab48017be4b3ed9d2d2498
mateusz <mateuszbronclik@gmail.com>
2023-03-03 14:43:32 +0100

    added query validation

--- RestaurantAPI/Startup.cs
+++ RestaurantAPI/Startup.cs
@@ -77,6 +77,7 @@ namespace RestaurantAPI
             services.AddScoped<RequestTimeMiddleware>();
             services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
             services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
+            services.AddScoped<IValidator<RestaurntQuery>, RestaurantQueryValidator>();
             services.AddSwaggerGen();
             
         }
